c2242403895ad9067c04db98c94cafb316d62948
Mark Jonse <103767830+mjonse90@users.noreply.github.com>
2022-08-15 17:25:39 -0500

    Add files via upload
    Set Up the Database and Flask

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+# 9.5.1: Set Up the Database and Flask
+# Import Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect existing database into model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Set up Flask
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-
-# export FLASK_APP=app.py
-
-# set FLASK_APP=app.py
-
-# flask run
-
-
-
